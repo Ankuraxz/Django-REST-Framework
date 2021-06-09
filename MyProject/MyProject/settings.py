a8d2c54c883aabcb343efb549c2832055317cb76
ANKUR <ankurvermaaxz@gmail.com>
2021-06-09 13:57:28 +0530

    Serialization

--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'API_basic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
